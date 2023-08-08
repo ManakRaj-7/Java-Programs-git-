ad4b0d4d9cad5522922d954bc2009e7caae0e549
Manak Raj <7manakraj@gmail.com>
2023-08-08 10:09:17 +0530

    added one more line in Diff.java

--- Diff.java
+++ Diff.java
@@ -3,5 +3,7 @@ public class Diff{
            int a1 = 5;
            int b1 = 2;
             System.out.println("The difference is " + (a1-b1))
+            System.out.println("The difference is " + (a1-b1) " evn after infinite trials")
+            
     }
 }
